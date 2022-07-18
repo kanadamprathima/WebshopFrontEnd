0896cf8b72181661934bbefd64077cea2d84c81d
prathima <kanadamprathima@gmail.com>
2022-07-18 16:55:12 +0200

    basic frontend setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
-import DetailsPage from "./Pages/DetailsPage";
-import HomePage from "./Pages/HomePage";
-import NavBar from "./components/NavBar";
+import DetailsPage from "./Pages/DetailsPage/index";
+import HomePage from "./Pages/HomePage/index";
+import NavBar from "./components/NavBar/index";
 
 function App() {
   return (
     <div className="App">
       <h1>WEBSHOP PROJECT</h1>
-      <NavBar />
+      <NavBar className="navbar" />
       <Routes>
-        <Route path="/HomePage" element={<HomePage />} />
-        <Route path="/DetailsPage" element={<DetailsPage />} />
+        <Route path="/HomePage/index" element={<HomePage />} />
+        <Route path="/DetailsPage/index" element={<DetailsPage />} />
       </Routes>
     </div>
   );
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,12 +3,12 @@ import { NavLink } from "react-router-dom";
 const NavBar = () => {
   return (
     <div>
-      <NavLink className="navItem" to="/HomePage">
+      <NavLink className="navItem" to="/HomePage/index">
         Home
       </NavLink>
-      {"-"}
-      <NavLink className="navItem" to="/DetailsPage">
-        Product details Page
+      {"  "}
+      <NavLink className="navItem" to="/DetailsPage/index">
+        Product Details page
       </NavLink>
     </div>
   );
