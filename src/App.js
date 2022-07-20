3279e275589825e080f0f74bca8df4ab83a1c1d3
chathu-weerasena <chathuweerasena@gmail.com>
2022-07-20 12:04:07 +0200

    product card

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import DetailsPage from "./Pages/DetailsPage/index";
 import HomePage from "./Pages/HomePage/index";
 import NavBar from "./components/NavBar/index";
 import ShopPage from "./Pages/ShopPage/index";
+//import BigBanner from "./components/BigBanner/index";
 //import "./my-sass.scss";
 
 function App() {
   return (
     <div className="App">
       <NavBar className="navbar" />
+      {/* <BigBanner className="banner" /> */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/details/:id" element={<DetailsPage />} />
